daa0879329be2361654f2a4a6967aadeffedb7a4
Cartaz <prenc.francesco@gmail.com>
2026-08-25 05:47:54 +0200

    Share the single QWebChannel backend proxy

---
+++ ui/web/backend-channel.js
@@ -0,0 +1,15 @@
+(() => {
+  "use strict";
+
+  const QWebChannelImpl = window.QWebChannel;
+  if (typeof QWebChannelImpl === "undefined") return;
+
+  window.QWebChannel = function FinanceTrackerQWebChannel(transport, callback) {
+    return new QWebChannelImpl(transport, (channel) => {
+      window.financeTrackerBackend = channel.objects.backend;
+      window.dispatchEvent(new Event("finance-backend-ready"));
+      callback(channel);
+    });
+  };
+  window.QWebChannel.prototype = QWebChannelImpl.prototype;
+})();
